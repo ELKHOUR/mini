6bf9431028c100bbf0984098e2493d13ea13c91e
elkhour <elkhour23@gmail.com>
2026-04-28 11:02:38 +0300

    fix: add src files properly

---
+++ src/models/enums/ResponseEnums.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+
+class ResponseSignal(Enum):
+    FILE_VALIDATED_SUCCESS = "file_validate_successfully"
+    FILE_TYPE_NOT_SUPPORTED = "File type not supported"
+    FILE_SIZE_EXCEEDED = "File size exceeded"
+    FILE_UPLOAD_SUCCESS = "file_upload_success"
+    FILE_UPLOAD_FAILED = "file_upload_failed"
+    PROCESSING_FAILED = "processing_failed"
+    PROCESSING_SUCCESS = "processing_success"
+    
+    
